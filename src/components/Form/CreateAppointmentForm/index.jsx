5f8465802b00e985870be8056734f05f84c2a524
Rodrigo Brazil <rodrigo.brazil15@gmail.com>
2021-04-23 03:05:35 -0300

    Connected axios api. Fixed createAppointmentForm component. Fixed table component.

--- src/components/Form/CreateAppointmentForm/index.jsx
+++ src/components/Form/CreateAppointmentForm/index.jsx
@@ -1,10 +1,25 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { setHours, setMinutes } from 'date-fns';
+import { toast } from 'react-toastify';
 import { DateInput } from '../../Input';
 import { appointmentFormSchema } from '../../../services/validations/forms';
+import axios from '../../../utils/api';
 
-const onSubmit = (values) => {
-  console.log('Form data', values);
+// const onSubmit = (values) => {
+//   console.log('Form data', values);
+// };
+
+const onSubmit = async (values) => {
+  try {
+    const response = await axios.post('/agendamento', {
+      name: values.name,
+      birthday: values.birthday,
+      schedulingDate: values.schedulingDate,
+    });
+    toast.success(response.data.message);
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
 };
 
 function CreateAppointmentForm() {
@@ -17,26 +32,31 @@ function CreateAppointmentForm() {
         birthday: null,
         schedulingDate: null,
       }}
-      render={({ isValid }) => (
+    >
+      {({ isValid }) => (
         <Form>
           <div className="form-group form-focus">
             <label className="focus-label" htmlFor="name">
-              Nome Completo
+              Nome Completo:
             </label>
             <Field className="form-control floating" name="name" type="text" />
             <ErrorMessage component="span" className="error" name="name" />
           </div>
           <div>
-            <DateInput name="birthday" label="Data de Nascimento:" />
+            <DateInput
+              name="birthday"
+              label="Data de Nascimento:"
+              maxDate={new Date()}
+            />
           </div>
           <div>
             <DateInput
               name="schedulingDate"
-              label="Selecione uma Data e Horário"
+              label="Selecione uma Data e Horário:"
               showTimeSelect
-              minDate={new Date()}
-              minTime={setHours(setMinutes(new Date(), 30), 7)}
-              maxTime={setHours(setMinutes(new Date(), 30), 17)}
+              minDate={new Date().setDate(new Date().getDate() + 1)}
+              minTime={setHours(setMinutes(new Date(), 0), 8)}
+              maxTime={setHours(setMinutes(new Date(), 0), 17)}
             />
           </div>
 
@@ -45,7 +65,7 @@ function CreateAppointmentForm() {
           </button>
         </Form>
       )}
-    />
+    </Formik>
   );
 }
 
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,80 +1,109 @@
+import { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
-
-const lista = [
-  {
-    name: 'rodrigo',
-    birthday: '2021-04-23T18:00:00.000Z',
-    schedulingDate: '2021-04-23T15:00:00.000Z',
-    checked: true,
-  },
-  {
-    name: 'rodrigo',
-    birthday: '2021-04-23T18:00:00.000Z',
-    schedulingDate: '2021-04-23T14:00:00.000Z',
-    checked: false,
-  },
-  {
-    name: 'rodrigo',
-    birthday: '2021-04-23T18:00:00.000Z',
-    schedulingDate: '2021-04-23T18:00:00.000Z',
-    checked: false,
-  },
-];
-
-const listaOrdenada = lista.sort(function (a, b) {
-  return new Date(a.schedulingDate) - new Date(b.schedulingDate);
-});
+import { toast } from 'react-toastify';
+import axios from '../../utils/api';
 
 function ScheduleTable() {
+  const [listaVac, setListaVac] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/agendamento');
+      setListaVac(
+        response.data.data.map((appointment) => ({
+          ...appointment,
+          birthday: new Date(appointment.birthday),
+          schedulingDate: new Date(appointment.schedulingDate),
+        }))
+      );
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleChecked = async ({ target: { checked } }, editVac) => {
+    const novaLista = listaVac.map((paciente) => {
+      if (paciente._id === editVac._id) {
+        return {
+          ...paciente,
+          check: checked,
+        };
+      }
+
+      return paciente;
+    });
+
+    try {
+      const response = await axios.put(`/agendamento/${editVac._id}`, {
+        ...editVac,
+        check: checked,
+      });
+      setListaVac(novaLista);
+      toast.info(response.data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const listaOrdenada = listaVac.sort((a, b) => {
+    return new Date(a.schedulingDate) - new Date(b.schedulingDate);
+  });
+
+  function maskDate(time) {
+    const date = new Date(time);
+    return date.toLocaleDateString(navigator.language, {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }
+
+  function maskTime(time) {
+    const date = new Date(time);
+    return date.toLocaleTimeString(navigator.language, {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+
   return (
-    <Table striped variant="dark">
+    <Table striped hover>
       <thead>
         <tr>
           <th>Nome</th>
-          <th>Nascimento</th>
+          <th>Data de Nascimento</th>
           <th>Data de Vascinação</th>
           <th>Situação</th>
         </tr>
       </thead>
       <tbody>
-        {listaOrdenada.map((elem) => (
-          <tr>
-            <td>{elem.name}</td>
-            <td>{elem.birthday}</td>
-            <td>{elem.schedulingDate}</td>
+        {listaOrdenada.map((paciente) => (
+          <tr key={paciente._id}>
+            <td>{paciente.name}</td>
+            <td>{maskDate(paciente.birthday)}</td>
             <td>
-              {elem.checked === true && (
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="flexSwitchCheckDefault"
-                    checked
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckDefault"
-                  >
-                    Default switch checkbox input
-                  </label>
-                </div>
-              )}
-
-              {elem.checked === false && (
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="flexSwitchCheckDefault"
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckDefault"
-                  >
-                    Default switch checkbox input
-                  </label>
-                </div>
-              )}
+              {maskDate(paciente.schedulingDate)} -{' '}
+              {maskTime(paciente.schedulingDate)}
+            </td>
+            <td>
+              <div className="form-check form-switch">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault"
+                  checked={paciente.check}
+                  onChange={(event) => handleChecked(event, paciente)}
+                />
+                <span className="m-0">
+                  {paciente.check
+                    ? ' Atendimento Realizado'
+                    : ' Aguardando Consulta'}
+                </span>
+              </div>
             </td>
           </tr>
         ))}
